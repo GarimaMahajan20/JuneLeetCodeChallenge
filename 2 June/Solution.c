968564e894882b30eb7796629bebbbc73d7615e9
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-06-02 16:29:41 +0530

    Add files via upload

---
+++ 2 June/Solution.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    
+    struct ListNode *dnode=NULL;
+    
+    if(node!=NULL)
+    {
+        node->val= node->next->val;
+    }
+    dnode=node->next;
+    node->next=dnode->next;
+    dnode->next=NULL;
+    free(dnode);
+    dnode=NULL;
+}
