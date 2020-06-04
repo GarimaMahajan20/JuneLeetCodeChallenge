e835ffc0ddc0be08ce6b2d3d850810f24bd02d12
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-06-04 12:39:28 +0530

    4 June

---
+++ 4 June/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int n=s.length-1;
+        int i=0;
+        
+        reverse(s,i,n);
+        System.out.println(s);
+        
+    }
+    
+    public void reverse(char []s,int i , int n)
+    {
+        char t='\0';
+        if(i<n)
+        {
+            t=s[i];
+            s[i]=s[n];
+            s[n]=t;
+            reverse(s,i+1,n-1);
+        }
+    }
+}
+
+//Using two pointer approach.
