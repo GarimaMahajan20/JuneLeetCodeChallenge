8afc4aa859357aa5dc5069697ca3a5583d116dba
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-06-08 13:01:55 +0530

    6 June

--- 6 June/Solution.java
+++ 6 June/Solution.java
@@ -1,5 +1,4 @@
 class Solution {
-    
     public int[][] reconstructQueue(int[][] people) {
         // sort array using Arrays.sort with custom comparator
         Arrays.sort(people, (p1, p2) -> p1[0] == p2[0] ? p1[1] - p2[1] : p2[0] - p1[0]); 
@@ -8,7 +7,6 @@ class Solution {
             list.add(people[i][1], people[i]); // placing people based on the K value
         }
         return list.toArray(people); // convert list to array
-    
     }
     
 }
