a413300e77c12d83bf830235911cef2913771989
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-06-09 13:44:31 +0530

    Java Solution

--- 9 June/Solution.java
+++ 9 June/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int j =0;
-        int i =0;
         if(s.length()==0){
             return true;
         }
         if(t.length()==0){
             return false;
         }
+        int j =0;
+        int i =0;
         while(j<t.length()&&i<s.length()){
             if(s.charAt(i)==t.charAt(j)){
                 i++;
