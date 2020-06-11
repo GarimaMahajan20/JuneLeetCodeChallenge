d97bb6fa2c8257d6018a23b21e00e08db4647b10
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-06-11 13:25:19 +0530

    11 June

---
+++ 11 June/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        int current = 0;
+        while(current <= right) {
+            if(nums[current] == 0){
+                int temp =nums[current];
+                nums[current]=nums[left];
+                nums[left]=temp;
+                left++;
+                current++;
+            }
+            else if(nums[current] == 1) {
+                current++;
+            }
+            else {
+                int temp =nums[current];
+                nums[current]=nums[right];
+                nums[right]=temp;                
+                right--;
+            }
+        }     
+    }
+}
+//Two pointer approach
